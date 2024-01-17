0fcd85c738cb153a856625f7cfc45b500f9420a0
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2024-01-17 16:13:41 +0000

    backend server and db, frontend fetch

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,63 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const router = require('./routes/router');
-const mongoose = require('mongoose');
-require('dotenv/config');
-
+const express = require("express");
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
+const cors = require("cors");
 const corsOptions = {
-	origin: '*',
+	origin: "*",
 	credentials: true,
-	optionSucessStatus: 200
+	optionsSuccessStatus: 200
 }
-
 app.use(cors(corsOptions));
 
-app.use('/', router);
+const bodyparser = require("body-parser");
+app.use(express.json());
+require("dotenv").config({ path: "./config.env" });
+const { MongoClient } = require("mongodb");
+const url =  process.env.DB_URI;
 
-const dbOptions = {useNewUrlParser:true, useUnifiedTopology:true}
-mongoose.connect(process.env.DB_URI, dbOptions);
+const router = express();
 
-try {
-	console.log("DB connected!!!!!!!!");
-} catch (err) {
-	console.log(err);
-}
+const ObjectId = require('mongodb').ObjectId;
 
+const client = new MongoClient(url);
+const dbName = "ourocean";
+const db = client.db(dbName);
+const newsCol = db.collection("news");
+const eventsCol = db.collection("events");
 
-// .then(() => console.log('DB connected!'));
-// .catch(err => console.log(err));
+client.connect();
 
+// routes 
+app.use("/", router);
 
+app.get("/news", async(req, res) => {
+	const getData = await newsCol.find({}).toArray();
+	res.send(getData);
+})
 
-const port = process.env.PORT || 4000;
+app.get("/news/:id", async(req, res) => {
+ 	let collection = await newsCol;
+ 	let query  = {_id: new ObjectId(req.params.id)};
+ 	let result = await collection.findOne(query);
+ 	if(!result) res.send("not found").status(404);
+ 	else res.send(result).status(200);
+});
+
+app.get("/events", async(req, res) => {
+	const getData = await eventsCol.find({}).toArray();
+	res.send(getData);
+});
+
+app.get("/events/:id", async(req, res) => {
+	let collection = await eventsCol;
+ 	let query  = {_id: new ObjectId(req.params.id)};
+ 	let result = await collection.findOne(query);
+ 	if(!result) res.send("not found").status(404);
+ 	else res.send(result).status(200);
+})
+
+// server
+const port = process.env.PORT;
 const server = app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 })
