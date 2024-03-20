2d9f26f8245f9eb55aab474fe3b2a4af4e5e4f48
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2024-03-20 12:15:48 +0000

    Home Image fullscreen effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,42 +11,50 @@ export default function Home() {
 	},[])
 	return(
 		<>	
+			
+			
 			<div className="firstHead">
-				<h1 className="homeHeader">Ocean water covers more than 70 percent of the Earth, 
+				<div className="firstHeadImageHolder"></div>
+				<h1 className="homeHeader firstHomeHeader">Ocean water covers more than 70 percent of the Earth, 
 				and only in recent decades have we begun to understand how humans impact this 
 				watery habitat.
 				</h1>
+				<div className="scroll">
+					<div className="scrollWheel"></div>
+				</div>
 			</div>
-			
-			<img className="homeImageOne" src={underwaterImg} alt="Clear below sea"/>
-			
 			<div className="secondHead">
-				<h1 className="homeHeader">Until fairly recently it was assumed that no matter 
+				<div className="secondHeadImageHolder"></div>
+				<h1 className="homeHeader secondHomeHeader">Until fairly recently it was assumed that no matter 
 				how much trash and chemicals humans dumped into the oceans, the effects would be 
 				negligible.
 				</h1>
+				<div className="secondScroll">
+					<div className="secondScrollWheel"></div>
+				</div>
 			</div>
-
-			<img 
-				className="homeImageTwo" src={pollutedImg} alt="pollution below the surface"/>
-
+			
 			<div className="thirdHead">
-				<h1 className="homeHeader">The many types of marine pollution affect entire ecosystems which in turn 
+				<h1 className="homeHeader thirdHomeHeader">The many types of marine pollution affect entire ecosystems which in turn 
 				affects us.
 				</h1>
-			</div>
-			
+				<div className="thirdHeadImageHolder"></div>
+				<div className="thirdScroll">
+					<div className="thirdScrollWheel"></div>
+				</div>
+				
 
-			<img className="homeImageThree" src={fishingVillage} alt="beautiful fishing village"/>
-			
+			</div>
+
 			<div className="fourthHead">
-				<h1 className="homeHeader">
+				<h1 className="homeHeader fourthHomeHeader">
 				If no action is taken, the plastic will increasingly impact our ecosystems, 
 				health, and economies. If we don’t take action collectively, we’ll all pay 
 				the price individually.
 				</h1>
 				<Button />
 			</div>
+		
 		</>
 	);
 }
